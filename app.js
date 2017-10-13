24efbb0c9dc3feedac1317068885a3f59a981592
Eric <ecomstock@gmail.com>
2017-10-13 16:02:47 -0700

    Created package.json and .gitignore. Also assigned view engine to EJS and established the port. Still need to edit 'Server listening...' line.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const http = require('http')
-const port = 8888
-const hostName = "localhost"
+
+const app = express()
+app.use(express.static('static'))
+app.set('view engine', 'ejs')
 
 let server = http.createServer((request, response) => {
 	response.end('Hello world')
 })
 
+const port = process.env.PORT || 3000
+const host = localhost
 server.listen(port, hostName)
 console.log("Server listening at http://localhost:8888/")
