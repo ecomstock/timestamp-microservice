590c639e19fac7175d02ff8da230afd7631d30e1
Eric <ecomstock@gmail.com>
2017-10-15 19:51:20 -0700

    Installed Pug, set as view engine, updated package.json, created index.pug, and added boilerplate html starter.

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const http = require('http')
 
 const app = express()
-app.use(express.static('static'))
-app.set('view engine', 'ejs')
-
-let server = http.createServer((request, response) => {
-	response.end('Hello world')
-})
+//app.use(express.static('static'))
+app.set('view engine', 'pug')
 
 const port = process.env.PORT || 3000
-const host = localhost
-server.listen(port, hostName)
-console.log("Server listening at http://localhost:8888/")
+const host = 'localhost'
+const server = http.createServer(app)
+server.listen(port, host, () => {
+	console.log(`Server listening at port ${port}`)
+})
