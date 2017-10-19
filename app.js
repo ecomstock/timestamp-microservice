2e41aaa200860c28879d41915449dc881c263269
Eric <ecomstock@gmail.com>
2017-10-18 19:25:06 -0700

    Completed JSON function and added a function to write the natural date.

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express')
 const http = require('http')
 
 const app = express()
-app.set('view engine', 'pug')
+//app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-	res.render('index')
+const port = process.env.PORT || 3000
+const host = 'localhost'
+
+app.listen(port, host, () => {
+	console.log(`Server listening at port ${port}.`)
 })
 
 // GET request for URL parameter. Declare timestamp variable and
 // set equal to the timestamp parameter. 
-app.get('/:timestamp', function(req, res) {
+app.get('/:timestamp', (req, res) => {
   let timestamp = req.params.timestamp;
-  res.send(`Timestamp: ${timestamp}`);
+  res.json(getJSON(timestamp));
 });
 
 function getJSON(timestamp) {
@@ -26,24 +29,38 @@ function getJSON(timestamp) {
 
 	// If timestamp is a number, convert it to a date.
 	// Else attempt to make a new date out of other possible formats.
-	if (!isNan(parseInt(timestamp))) {
+	if (!isNaN(parseInt(timestamp))) {
 		date = new Date(parseInt(timestamp))
 	} else {
 		date = new Date(timestamp)
 	}
 
-	// Convert date back to unix time and store in result.unix
-	if (!isNan(date.getTime())) {
+	// Convert date back to unix time and store in result.unix.
+	// Run the date object through a function that makes it natural and 
+	// store it in result.natural.
+	if (!isNaN(date.getTime())) {
 		result.unix = date.getTime()
-		result.natural =
+		result.natural = getNaturalDate(date)
 	}
 
 	return result;
 }
 
-const port = process.env.PORT || 3000
-const host = 'localhost'
-
-app.listen(port, host, () => {
-	console.log(`Server listening at port ${port}.`)
-})
+function getNaturalDate(date) {
+	var months = [
+	'January', 
+	'February', 
+	'March', 
+	'April', 
+	'May', 
+	'June', 
+	'July', 
+	'August', 
+	'September', 
+	'Obtober', 
+	'November', 
+	'December'
+	];
+ 
+	return `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`
+}
